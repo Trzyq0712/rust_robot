bff0c20d0d1031f55615e546fbad89513e4616e6
Trzyq0712 <j.trzyq@gmail.com>
2022-12-25 22:33:53 +0100

    Switch to pac crate, enable interrupts

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,79 @@
-#![allow(unused)]
 #![no_main]
 #![no_std]
+#![allow(unused)]
+
+use core::cell::{Cell, RefCell};
+use core::sync::atomic::{AtomicU32, Ordering};
 
 // Halt on panic
-use core::f32::consts::FRAC_PI_2;
-use cortex_m::asm::delay;
 use panic_halt as _; // panic handler
 
-use cortex_m_rt::entry;
-use micromath::F32Ext;
-use stm32f4xx_hal as hal;
+use cortex_m::{asm, interrupt as intr, interrupt::Mutex};
+use cortex_m_rt::{entry, exception};
+use stm32::interrupt;
+use stm32f4::stm32f401 as stm32;
 
-use crate::hal::timer::Channel;
-use crate::hal::{pac, prelude::*};
 use rtt_target::{rprintln, rtt_init_print};
+
+static G_TIM2: Mutex<RefCell<Option<stm32::TIM2>>> = Mutex::new(RefCell::new(None));
+static G_INTR_COUNTER: AtomicU32 = AtomicU32::new(0);
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("Hello2");
 
-    let dp = pac::Peripherals::take().unwrap();
-    let gpioc = dp.GPIOC.split();
-    let mut led = gpioc.pc13.into_push_pull_output();
+    let dp = stm32::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
 
-    let gpiob = dp.GPIOB.split();
-    let gpioa = dp.GPIOA.split();
-    let mut left_motor_fd = gpiob.pb15.into_push_pull_output();
-    let mut left_motor_bk = gpiob.pb14.into_push_pull_output();
-    let mut right_motor_bk = gpiob.pb3.into_push_pull_output();
-    let mut right_motor_fd = gpioa.pa8.into_push_pull_output();
+    dp.RCC.ahb1enr.modify(|_, w| w.gpiocen().enabled());
+    dp.RCC.apb1enr.modify(|_, w| w.tim2en().enabled());
 
-    // Set up the system clock. We want to run at 48MHz for this one.
-    let rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.use_hse(25.MHz()).sysclk(48.MHz()).freeze();
+    dp.GPIOC.moder.modify(|_, w| w.moder13().output());
+    dp.GPIOC.otyper.modify(|_, w| w.ot13().push_pull());
 
-    // Create a delay abstraction based on general-pupose 32-bit timer TIM5
-    let mut delay = dp.TIM5.delay_us(&clocks);
+    intr::free(|cs| {
+        dp.TIM2.arr.write(|w| w.arr().bits(0xFF));
+        dp.TIM2.cr1.write(|w| {
+            w.urs().counter_only();
+            w.arpe().enabled();
+            w.cen().enabled()
+        });
+        dp.TIM2.dier.modify(|_, w| w.uie().enabled());
+        dp.TIM2.psc.write(|w| w.psc().bits(0xFFFF));
+        dp.TIM2.egr.write(|w| w.ug().update());
 
-    // let channels = (
-    //     gpiob.pb0.into_alternate::<2>(),
-    //     gpiob.pb1.into_alternate::<2>(),
-    // );
+        G_TIM2.borrow(cs).replace(Some(dp.TIM2));
+    });
 
-    // let mut pwm = dp.TIM3.pwm_us(channels, 0xFF.micros(), &clocks);
-    // let max_duty = pwm.get_max_duty();
-
-    // rprintln!("Max duty: {}", &max_duty);
-
-    // pwm.enable(Channel::C3);
-    // pwm.enable(Channel::C4);
-
-    left_motor_fd.set_high();
-    left_motor_bk.set_low();
-    right_motor_fd.set_high();
-    right_motor_bk.set_low();
-
-    loop {
-        cortex_m::asm::nop();
-        delay.delay(1.secs());
-        led.toggle();
+    unsafe {
+        stm32::NVIC::unmask(stm32::interrupt::TIM2);
     }
+    loop {
+        rprintln!("Loop");
+        dp.GPIOC.odr.modify(|r, w| w.odr13().bit(!r.odr13().bit()));
+        rprintln!(
+            "TIM2 cnt: {}",
+            intr::free(|cs| G_TIM2
+                .borrow(cs)
+                .borrow()
+                .as_ref()
+                .unwrap()
+                .cnt
+                .read()
+                .bits())
+        );
+        rprintln!("Counter: {}", G_INTR_COUNTER.load(Ordering::Relaxed));
+        asm::delay(5_000_000);
+    }
+}
+
+#[interrupt]
+fn TIM2() {
+    rprintln!("Interrupt");
+    G_INTR_COUNTER.fetch_add(1, Ordering::Relaxed);
+    intr::free(|cs| {
+        let mut tim2 = G_TIM2.borrow(cs).borrow_mut();
+        tim2.as_mut().unwrap().sr.modify(|_, w| w.uif().clear_bit());
+    });
 }
